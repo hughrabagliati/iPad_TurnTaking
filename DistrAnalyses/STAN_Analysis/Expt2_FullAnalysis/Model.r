1c8e8db02853bb8876d1c0213af4d408a3c38afe
hughrabagliati <hugh.rabagliati@gmail.com>
2016-12-18 09:53:37 +0000

    models in one file

--- DistrAnalyses/STAN_Analysis/Expt2_FullAnalysis/Model.r
+++ DistrAnalyses/STAN_Analysis/Expt2_FullAnalysis/Model.r
@@ -53,23 +53,51 @@ ggplot(tt,aes(x=rt,..density..,col=Pred))+ geom_freqpoly(alpha=1,lwd =1.5, bins 
 # Fit Ex-Gaussian using ML (retimes library) 
 eg_ml <- timefit(tt$rt)
 print(eg_ml)
+
 stanDat_full <- list(rt = tt$rt,
                      factor1 = tt$N_Early,
                      factor2 = tt$N_Pred,
-                    # factor3 = tt$N_AgeFive,
-                     factor4 = tt$N_AgeThree, 
-                     factor5 = tt$N_E_P_Interact, 
-                    # factor6 = tt$N_Early_AgeFive_Interact, 
-                     factor6a = tt$N_Early_AgeThree_Interact, 
-                     # factor7 = tt$N_Pred_AgeFive_Interact, 
-                     factor7a = tt$N_Pred_AgeThree_Interact, 
-                    # factor8 = tt$N_Early_Pred_AgeFive_Interact, 
-                     factor8a = tt$N_Early_Pred_AgeThree_Interact, 
+                     factor3 = tt$N_Right,
+                     factor4 = tt$N_AgeFive,
+                     factor5 = tt$N_AgeThree, 
+                     factor6 = tt$N_E_P_Interact, 
+                     factor7 = tt$N_Early_AgeFive_Interact, 
+                     factor7a = tt$N_Early_AgeThree_Interact, 
+                     factor8 = tt$N_Pred_AgeFive_Interact, 
+                     factor8a = tt$N_Pred_AgeThree_Interact, 
+                     factor9 = tt$N_Early_Pred_AgeFive_Interact, 
+                     factor9a = tt$N_Early_Pred_AgeThree_Interact, 
                      N = nrow(tt), J = nlevels(as.factor(tt$Subject)), Subj = as.integer(as.factor(tt$Subject)))
 
-eg_stan_exp <- stan(file="fixEf_Age_and_Conds_transf_expt2.stan",
+eg_stan_exp2 <- stan(file="fixEf_AllAges_and_Conds_transf_expt2_beta.stan",
                     data=stanDat_full,
-                    chains = 3, iter = 50,  control = list(adapt_delta = 0.88))
+                    chains = 3, iter = 750,  control = list(adapt_delta = 0.95))
+
+tt_adult <- subset(tt, Age == "Adult")
+
+stanDat_adult <- list(rt = tt_adult$rt,
+                      factor1 = tt_adult$N_Early,
+                      factor2 = tt_adult$N_Pred,
+                      factor3 = tt_adult$N_Right,
+                      factor4 = tt_adult$N_E_P_Interact, 
+                      N = nrow(tt_adult), J = nlevels(as.factor(tt_adult$Subject)), Subj = as.integer(as.factor(tt_adult$Subject)))
+
+adult_model2 <- stan(file="fixEf_Conds_transf_expt2.stan",
+                    data=stanDat_adult,
+                    chains = 3, iter = 750,  control = list(adapt_delta = 0.95))
+
+tt_kids <- subset(tt, Age != "Adult")
+
+stanDat_kids <- list(rt = tt_kids$rt,
+                     factor1 = tt_kids$N_Early,
+                     factor2 = tt_kids$N_Pred,
+                     factor3 = tt_kids$N_Right,
+                     factor4 = tt_kids$N_E_P_Interact, 
+                     N = nrow(tt_kids), J = nlevels(as.factor(tt_kids$Subject)), Subj = as.integer(as.factor(tt_kids$Subject)))
+
+kid_model <- stan(file="fixEf_Conds_transf_expt2.stan",
+                  data=stanDat_kids,
+                  chains = 3, iter = 750,  control = list(adapt_delta = 0.95))
 
 
 print(eg_stan_exp, pars = c("beta0","beta","beta_s0","beta_s","beta_t0","beta_t"), probs = c(0.025,0.5,0.975))
